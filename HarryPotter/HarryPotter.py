5dabb4f5da863ca124f89319e4b84562bd3d21cb
terryyin <terry@odd-e.com>
2013-07-07 10:36:59 +0800

    a full solution with bad performance

--- HarryPotter/HarryPotter.py
+++ HarryPotter/HarryPotter.py
@@ -1,15 +1,20 @@
 import unittest
+from itertools import combinations
 
 class Book:
     pass
 
-def costOfBook(*books):
+def costOfBooks(*books):
     if len(books) == 0:
         return 0
+    return min(costsOfPossibleCombinations(books))
+
+def costsOfPossibleCombinations(books):
     bookSet = set(books)
-    cost = [0, 8, 7.6, 7.2, 6.4, 6][len(bookSet)]
-    
-    return cost * len(bookSet) + costOfBook(*restBooks(books, bookSet))
+    for n in range(1, len(bookSet)+1): 
+        for subSet in combinations(bookSet, n) :             
+            yield [0, 8, 7.6 * 2, 7.2 * 3, 6.4 * 4, 6 * 5][n]\
+                + costOfBooks(*restBooks(books, subSet))
 
 def restBooks(books, bookSet):
     bookList = list(books)
@@ -20,32 +25,40 @@ def restBooks(books, bookSet):
 class Test(unittest.TestCase):
 
     def testOneBookCost8Dollors(self):
-        self.assertEqual(8, costOfBook(Book()))
+        self.assertEqual(8, costOfBooks(Book()))
 
     def testTwoSameBooksHaveNoDiscount(self):
         book = Book()
-        self.assertEqual(8 * 2, costOfBook(book, book))
+        self.assertEqual(8 * 2, costOfBooks(book, book))
 
     def testTwoDifferentBooksHas5PercentDiscount(self):
-        self.assertEqual(2 * 8 * 0.95, costOfBook(Book(), Book()))
+        self.assertEqual(2 * 8 * 0.95, costOfBooks(Book(), Book()))
 
     def testThreeDifferentBooksHas10PercentDiscount(self):
-        self.assertEqual(3 * 8 * 0.90, costOfBook(Book(), Book(), Book()))
+        self.assertEqual(3 * 8 * 0.90, costOfBooks(Book(), Book(), Book()))
 
     def testFourDifferentBooksHas20PercentDiscount(self):
-        self.assertEqual(4 * 8 * 0.80, costOfBook(Book(), Book(), Book(), Book()))
+        self.assertEqual(4 * 8 * 0.80, costOfBooks(Book(), Book(), Book(), Book()))
 
     def testFiveDifferentBooksHas25PercentDiscount(self):
-        self.assertEqual(5 * 8 * 0.75, costOfBook(Book(), Book(), Book(), Book(), Book()))
+        self.assertEqual(5 * 8 * 0.75, costOfBooks(Book(), Book(), Book(), Book(), Book()))
 
     def testTwoSameBooksAndOneDifferentBookThenOneOfTheSameBookHasNoDiscount(self):
         book = Book()
-        self.assertEqual(8 * 2 * 0.95 + 8, costOfBook(book, Book(), book))
+        self.assertEqual(8 * 2 * 0.95 + 8, costOfBooks(book, Book(), book))
 
     def testTwoSetOfTwoDifferentBooksHas5PercentDiscount(self):
         book1 = Book()
         book2 = Book()
-        self.assertEqual(8 * 4 * 0.95, costOfBook(book1, book1, book2, book2))
+        self.assertEqual(8 * 4 * 0.95, costOfBooks(book1, book1, book2, book2))
+
+    def testUseBiggestDiscount(self):
+        book1 = Book()
+        book2 = Book()
+        book3 = Book()
+        book4 = Book()
+        book5 = Book()
+        self.assertEqual(2 * 4 * 6.4, costOfBooks(book1, book1, book2, book2, book3, book3, book4, book5))
 
 
 
