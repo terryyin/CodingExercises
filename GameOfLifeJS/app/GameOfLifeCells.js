00d5d08cc2dd89f37aef51b919b93e70702848bb
Terry Yin <terry.yinzhe@gmail.com>
2017-09-01 16:20:54 +0800

    done

---
+++ GameOfLifeJS/app/GameOfLifeCells.js
@@ -0,0 +1,53 @@
+function GameOfLife(cells) {
+  this.cells = cells || new Array();
+}
+
+GameOfLife.prototype.setAlives = function(cells){
+  this.cells = this.cells.concat(cells);
+};
+
+GameOfLife.prototype.isAlive = function(cell){
+  return this.cells.some((c)=>{ return c.equals(cell);});
+};
+
+GameOfLife.prototype.next = function(){
+  return new GameOfLife(
+    this.cellsWithNeighbourRange(this.cells, [2, 3]).concat(
+    this.cellsWithNeighbourRange(this.allNeighbours(), [3])));
+};
+
+GameOfLife.prototype.allNeighbours = function(){
+  return this.cells.reduce((a, c)=>{
+    return a.concat(c.neighbours()); }, []);
+}
+
+GameOfLife.prototype.cellsWithNeighbourRange = function(cells, range){
+  return cells.filter((c)=>{
+    return range.includes(this.numberOfAliveNeighbours(c)); });
+}
+
+GameOfLife.prototype.numberOfAliveNeighbours = function(cell){
+  return cell.neighbours().filter((n)=>{
+    return this.isAlive(n) }).length;
+}
+
+function Cell(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Cell.prototype.toString = function(){
+  return '(' + [this.x, this.y].join(', ') + ')';
+};
+
+Cell.prototype.neighbours = function(){
+  return [
+    [-1, 0], [1, 0], [0, 1], [0, -1], [1, 1], [-1, 1], [1, -1], [-1, -1]
+  ].map((d)=> {return  new Cell(this.x + d[0], this.y + d[1]) });
+};
+
+Cell.prototype.equals = function(other){
+  return this.x === other.x && this.y === other.y;
+};
+
+module.exports = { GameOfLife: GameOfLife, Cell: Cell };
--- GameOfLifeJS/spec/GameOfLifeSpec.js
+++ GameOfLifeJS/spec/GameOfLifeSpec.js
@@ -1,68 +1,46 @@
-beforeEach( ()=> {
-  jasmine.addMatchers({
-    toBeDeadAt: ()=> {
-      return {
-        compare: function (game, cell) {
-          return {
-            pass: !game.isAlive(cell)
-          };
-        }
-      };
-    }
-  });
-});
+var GameOfLife = require('../app/GameOfLifeCells').GameOfLife;
+var Cell = require('../app/GameOfLifeCells').Cell;
 
-
-var GameOfLife = require('../app/GameOfLife').GameOfLife;
-var Cell = require('../app/GameOfLife').Cell;
-
-describe('An alive cell', ()=> {
-  var the_cell = new Cell(3, 2);
+describe('In the Game of life, ', ()=> {
+  var the_cell = new Cell(8, 2);
+  var neighbours = Object.values(the_cell.neighbours());
   var game;
 
-  beforeEach(()=> {
-    game = new GameOfLife();
-    game.setAlive(the_cell);
+  beforeEach(()=> { game = new GameOfLife(); });
+
+  describe('An alive cell', ()=> {
+    beforeEach(()=> { game.setAlives([the_cell]); });
+
+    [
+      [0, false],
+      [1, false],
+      [2, true],
+      [3, true],
+      [4, false]
+    ].forEach((param)=> {
+      it('should '+(param[1] ? '' : 'not') +' be alive with ' + param[0]+' neighbours', ()=> {
+        game.setAlives(neighbours.slice(0, param[0]));
+        expect(game.next().isAlive(the_cell)).toBe(param[1]);
+      });
+    });
   });
 
-  it('should be alive if set alive', ()=> {
-    expect(game).not.toBeDeadAt(new Cell(3, 2));
+  describe('A dead cell', ()=> {
+    it('should revive with 3 neighbours', ()=> {
+      game.setAlives(neighbours.slice(0, 3));
+      expect(game.next().isAlive(the_cell)).toBe(true);
+    });
   });
 
-  it('should be killed when no neighbour', ()=> {
-    expect(game.next()).toBeDeadAt(the_cell);
-  });
-
-  it('should survive with two live neighbhours', ()=> {
-    game.setAlive(the_cell.neighbours().left);
-    game.setAlive(the_cell.neighbours().right);
-    expect(game.next()).not.toBeDeadAt(the_cell);
-    expect(game.next()).toBeDeadAt(the_cell.neighbours().right);
-    expect(game.next()).toBeDeadAt(the_cell.neighbours().left);
-  });
-
-  it('should survive with three live neighbhours', ()=> {
-    game.setAlive(the_cell.neighbours().left);
-    game.setAlive(the_cell.neighbours().right);
-    game.setAlive(the_cell.neighbours().up);
-    expect(game.next()).not.toBeDeadAt(the_cell);
-  });
-
-  it('should be killed with four live neighbhours', ()=> {
-    game.setAlive(the_cell.neighbours().left);
-    game.setAlive(the_cell.neighbours().right);
-    game.setAlive(the_cell.neighbours().up);
-    game.setAlive(the_cell.neighbours().down);
-    expect(game.next()).toBeDeadAt(the_cell);
-  });
 });
 
 describe('A cell', ()=> {
   var cell = new Cell(3, 2);
 
-  xit('s neighours should include the one to its left', ()=> {
-    var cell = new Cell(3, 2);
-    expect(cell.neighbours()).toInclude(new Cell(2, 2));
+  it('has neighbours', ()=> {
+    expect(cell.neighbours()[4].equals(new Cell(4, 3))).toBe(true);
+    expect(cell.neighbours()[5].equals(new Cell(2, 3))).toBe(true);
+    expect(cell.neighbours()[6].equals(new Cell(4, 1))).toBe(true);
+    expect(cell.neighbours()[7].equals(new Cell(2, 1))).toBe(true);
   });
-
-});
+})
