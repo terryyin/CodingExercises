37cac89ff7480d162687c366dbd020c50efb280d
Terry Yin <terry.yinzhe@gmail.com>
2017-11-23 15:13:29 +0800

    ones add I

--- add_romans_in_go/roman.go
+++ add_romans_in_go/roman.go
@@ -1,5 +1,13 @@
 package roman
 
 func romanAdd(left string, right string) string {
-	return map[string]string{"I": "II", "II": "III", "III": "IV"}[left]
+	ones := []string{"II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}
+	ones_plus_one := map[string]string{}
+	key := "I"
+	for _, val := range ones {
+		ones_plus_one[key] = val
+		key = val
+	}
+
+	return ones_plus_one[left]
 }
--- add_romans_in_go/roman_test.go
+++ add_romans_in_go/roman_test.go
@@ -2,12 +2,24 @@ package roman
 
 import "fmt"
 
-func ExampleAddI() {
+func ExampleIsAddI() {
 	fmt.Println(romanAdd("I", "I"))
 	fmt.Println(romanAdd("II", "I"))
 	fmt.Println(romanAdd("III", "I"))
+	fmt.Println(romanAdd("IV", "I"))
+	fmt.Println(romanAdd("V", "I"))
+	fmt.Println(romanAdd("VI", "I"))
+	fmt.Println(romanAdd("VII", "I"))
+	fmt.Println(romanAdd("VIII", "I"))
+	fmt.Println(romanAdd("IX", "I"))
 	// Output:
 	// II
 	// III
 	// IV
+	// V
+	// VI
+	// VII
+	// VIII
+	// IX
+	// X
 }
