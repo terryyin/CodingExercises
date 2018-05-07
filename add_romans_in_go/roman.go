a4996c57a328f5ea70aba81034a673c93e28d712
Terry Yin <terry.yinzhe@gmail.com>
2018-05-07 20:43:38 +0800

    go exercise

--- add_romans_in_go/roman.go
+++ add_romans_in_go/roman.go
@@ -2,45 +2,19 @@ package roman
 
 import "strings"
 
-func romanAdd(left string, right string) string {
-	if right == "" {
-		return left
+func ToRoman(number int) string {
+	romanDigit := map[int]string{
+		4:  "IV",
+		5:  "V",
+		9:  "IX",
+		10: "X",
 	}
-	if right == "IV" {
-		left = romanAdd(left, right[0:len(right)-2]+"III")
-		right = "I"
-	} else if right == "V" {
-		left = romanAdd(left, right[0:len(right)-1]+"IV")
-		right = "I"
-	} else {
-		left = romanAdd(left, right[0:len(right)-1])
-		right = "I"
+	if val, ok := romanDigit[number]; ok {
+		return val
 	}
 
-	return romanIncrease(left, right)
-}
-
-func romanIncrease(left string, right string) string {
-	ones_plus_one := []struct{ base, result string }{}
-	if right == "C" {
-		ones_plus_one = baseIncreaseTable("C", "D", "M")
-	} else if right == "X" {
-		ones_plus_one = baseIncreaseTable("X", "L", "C")
-	} else if right == "I" {
-		ones_plus_one = baseIncreaseTable("I", "V", "X")
+	if number > 5 {
+		return romanDigit[5] + ToRoman(number-5)
 	}
-	for _, pair := range ones_plus_one {
-		if strings.Contains(left, pair.base) {
-			return romanIncrease(strings.Replace(left, pair.base, "", 1), pair.result)
-		}
-	}
-	return left + right
-}
-
-func baseIncreaseTable(single string, five string, ten string) []struct{ base, result string } {
-	return []struct{ base, result string }{
-		{single + ten, ten},
-		{five + single + single + single, single + ten},
-		{single + five, five},
-		{single + single + single, single + five}}
+	return strings.Repeat("I", number)
 }
--- add_romans_in_go/roman_test.go
+++ add_romans_in_go/roman_test.go
@@ -2,17 +2,12 @@ package roman
 
 import "fmt"
 
-func ExampleIsAddI() {
-	fmt.Println(romanAdd("I", "I"))
-	fmt.Println(romanAdd("II", "I"))
-	fmt.Println(romanAdd("III", "I"))
-	fmt.Println(romanAdd("IV", "I"))
-	fmt.Println(romanAdd("V", "I"))
-	fmt.Println(romanAdd("VI", "I"))
-	fmt.Println(romanAdd("VII", "I"))
-	fmt.Println(romanAdd("VIII", "I"))
-	fmt.Println(romanAdd("IX", "I"))
+func ExampleOfRomanNumbers() {
+	for number := 1; number <= 10; number++ {
+		fmt.Println(ToRoman(number))
+	}
 	// Output:
+	// I
 	// II
 	// III
 	// IV
@@ -22,74 +17,4 @@ func ExampleIsAddI() {
 	// VIII
 	// IX
 	// X
-}
-
-func ExampleXAddI() {
-	fmt.Println(romanAdd("X", "I"))
-	fmt.Println(romanAdd("XI", "I"))
-	fmt.Println(romanAdd("XII", "I"))
-	fmt.Println(romanAdd("XIII", "I"))
-	fmt.Println(romanAdd("XIV", "I"))
-	fmt.Println(romanAdd("XIX", "I"))
-	fmt.Println(romanAdd("XX", "I"))
-	// Output:
-	// XI
-	// XII
-	// XIII
-	// XIV
-	// XV
-	// XX
-	// XXI
-}
-
-func ExampleXsAddI() {
-	fmt.Println(romanAdd("XXXIX", "I"))
-	fmt.Println(romanAdd("XLIX", "I"))
-	fmt.Println(romanAdd("LXXXIX", "I"))
-	fmt.Println(romanAdd("XCIX", "I"))
-	// Output:
-	// XL
-	// L
-	// XC
-	// C
-}
-
-func ExampleCsAddI() {
-	fmt.Println(romanAdd("CCCXCIX", "I"))
-	fmt.Println(romanAdd("CMXCIX", "I"))
-	// Output:
-	// CD
-	// M
-}
-
-func ExampleAddII() {
-	fmt.Println(romanAdd("I", "II"))
-	fmt.Println(romanAdd("II", "II"))
-	// Output:
-	// III
-	// IV
-}
-
-func ExampleAddIII() {
-	fmt.Println(romanAdd("I", "III"))
-	// Output:
-	// IV
-}
-
-func ExampleAddIV() {
-	fmt.Println(romanAdd("I", "IV"))
-	// Output:
-	// V
-}
-
-func ExampleAddV() {
-	fmt.Println(romanAdd("I", "V"))
-	// Output:
-	// VI
-}
-
-func ExampleAddVI() {
-	fmt.Println(romanAdd("I", "VI"))
-	// Output:
-	// VII
 }
