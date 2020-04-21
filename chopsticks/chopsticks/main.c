b1721a0fae61aa8fdd3e9a98fb45edf8255883ec
Terry Yin <terry.yinzhe@gmail.com>
2020-04-21 17:03:21 +0800

    refactoring test facilities

--- chopsticks/chopsticks/main.c
+++ chopsticks/chopsticks/main.c
@@ -10,9 +10,9 @@
 #include <assert.h>
 
 typedef struct TestCase {
-    int people_count;
-    int chopstick_count;
-    int chopstick_lengths[5000];
+  int people_count;
+  int chopstick_count;
+  int chopstick_lengths[5000];
 } TestCase;
 
 int solve_one_case(TestCase * test_case) {
@@ -27,19 +27,21 @@ int solve_one_case(TestCase * test_case) {
 }
 
 int solver(int total, TestCase * test_cases) {
-    return solve_one_case(test_cases);;
+  return solve_one_case(test_cases);;
 }
 
-TestCase * people_chopsticks(int p, int n, TestCase * result) {
-    TestCase tc = {p,n,{5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5}};
-    *result = tc;
-    return result;
+TestCase * for_people(int p, TestCase * result) {
+  TestCase tc = {p,0,{}};
+  *result = tc;
+  return result;
 }
 
-TestCase * shorter(int n, TestCase * result) {
-    for(int i=0; i < n; i++)
-      result->chopstick_lengths[i] = 4;
-    return result;
+TestCase * push(int n, int length, TestCase * result) {
+  int i = result->chopstick_count;;
+  for(; i < result-> chopstick_count + n; i++)
+    result->chopstick_lengths[i] = length;
+  result->chopstick_count = i;
+  return result;
 }
 
 void expect_eq(int expect, int actual, const char * message) {
@@ -49,16 +51,17 @@ void expect_eq(int expect, int actual, const char * message) {
 }
 
 void test_all() {
-    TestCase tc;
-    expect_eq(0, solver(1, people_chopsticks(1, 3, &tc)), "all same");
-    expect_eq(1, solver(1, shorter(1, people_chopsticks(1, 3, &tc))), "one shorter");
-    expect_eq(0, solver(1, shorter(1, people_chopsticks(1, 4, &tc))), "one shorter but there are more");
-    expect_eq(0, solver(1, shorter(2, people_chopsticks(1, 4, &tc))), "two short two long");
-    printf("Done.\n");
-    return;
+  TestCase tc;
+  expect_eq(0, solver(1, push(3, 5, for_people(1, &tc))), "all same");
+  expect_eq(1, solver(1, push(2, 5, push(1, 4, for_people(1, &tc)))), "one shorter");
+  expect_eq(0, solver(1, push(3, 5, push(1, 4, for_people(1, &tc)))), "one shorter but there are more");
+  expect_eq(0, solver(1, push(2, 5, push(2, 4, for_people(1, &tc)))), "two short two long");
+
+  printf("Done.\n");
+  return;
 }
 
 int main(int argc, const char * argv[]) {
-    test_all();
-    return 0;
+  test_all();
+  return 0;
 }
