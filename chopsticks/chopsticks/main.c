9ed959e337c914a465a490e3eb02f20848827b20
Terry Yin <terry.yinzhe@gmail.com>
2020-04-22 14:05:54 +0800

    seems done

--- chopsticks/chopsticks/main.c
+++ chopsticks/chopsticks/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <assert.h>
+#include <string.h>
 
 typedef struct TestCase {
   int people_count;
@@ -31,19 +32,16 @@ int neighbour_square(int i, TestCase * tc) {
 }
 
 int find_pair(int p, int start, TestCase * tc) {
+  if(tc->cache[p][start] != 0) return tc->cache[p][start];
   if(p >= tc->people_count) return 0;
   int result = 2147483647;
   int max = tc->chopstick_count - (tc->people_count - p) * 3;
   int next_p;
   for(int i = start; i <= max; i++) {
     int d = neighbour_square(i, tc);
-    if(tc->cache[p+1][i+2] != 0)
-      d += tc->cache[p+1][i+2];
-    else {
-      if (d >= result)
-        continue;
-      d += find_pair(p + 1, i+2, tc);
-    }
+    if (d >= result)
+      continue;
+    d += find_pair(p + 1, i+2, tc);
     if (result > d){
       result = d;
     }
@@ -51,13 +49,8 @@ int find_pair(int p, int start, TestCase * tc) {
   return tc->cache[p][start] = result;
 }
 
-int solve_one_case(TestCase * test_case) {
-  // print(test_case);
+int solver(TestCase * test_case) {
   return find_pair(0, 0, test_case);
-}
-
-int solver(int total, TestCase * test_cases) {
-  return solve_one_case(test_cases);;
 }
 
 TestCase * example(TestCase * result, int people_count, int chopstick_count, ...) {
@@ -81,29 +74,42 @@ void expect_eq(int expect, int actual, const char * message) {
 
 void test_all() {
   TestCase tc;
-  expect_eq(1, solver(1, example(&tc, 1, 3, /**/ 4, 5, 5)), "one shorter");
-  expect_eq(0, solver(1, example(&tc, 1, 3, /**/ 5, 5, 5)), "all same");
-  expect_eq(0, solver(1, example(&tc, 1, 4, /**/ 4, 5, 5, 5)), "one shorter but there are more");
-  expect_eq(0, solver(1, example(&tc, 1, 4, /**/ 4, 4, 5, 5)), "two short two long");
+  expect_eq(1, solver(example(&tc, 1, 3, /**/ 4, 5, 5)), "one shorter");
+  expect_eq(0, solver(example(&tc, 1, 3, /**/ 5, 5, 5)), "all same");
+  expect_eq(0, solver(example(&tc, 1, 4, /**/ 4, 5, 5, 5)), "one shorter but there are more");
+  expect_eq(0, solver(example(&tc, 1, 4, /**/ 4, 4, 5, 5)), "two short two long");
 
-  expect_eq(1, solver(1, example(&tc, 2, 6, /**/ 4, 4, 4, 5, 6, 6)), "two people and 2n has different length");
-  expect_eq(0, solver(1, example(&tc, 2, 6, /**/ 4, 4, 4, 4, 5, 5)), "two people and 2n has also same length");
-  expect_eq(1, solver(1, example(&tc, 2, 6, /**/ 1, 2, 4, 4, 4, 4)), "two people only 2n has same length");
-  expect_eq(0, solver(1, example(&tc, 2, 6, /**/ 1, 1, 2, 2, 7, 100)), "pairs");
-  expect_eq(2, solver(1, example(&tc, 2, 6, /**/ 1, 2, 2, 3, 7, 100)), "pairs should'nt take");
-  expect_eq(4, solver(1, example(&tc, 2, 7, /**/ 1, 5, 5, 6, 8, 100, 200)), "pairs overlapping (5,5) (5,6)");
-  expect_eq(2, solver(1, example(&tc, 2, 7, /**/ 4, 5, 5, 6, 8, 100, 200)), "2nd pair has better option");
+  expect_eq(1, solver(example(&tc, 2, 6, /**/ 4, 4, 4, 5, 6, 6)), "two people and 2n has different length");
+  expect_eq(0, solver(example(&tc, 2, 6, /**/ 4, 4, 4, 4, 5, 5)), "two people and 2n has also same length");
+  expect_eq(1, solver(example(&tc, 2, 6, /**/ 1, 2, 4, 4, 4, 4)), "two people only 2n has same length");
+  expect_eq(0, solver(example(&tc, 2, 6, /**/ 1, 1, 2, 2, 7, 100)), "pairs");
+  expect_eq(2, solver(example(&tc, 2, 6, /**/ 1, 2, 2, 3, 7, 100)), "pairs should'nt take");
+  expect_eq(4, solver(example(&tc, 2, 7, /**/ 1, 5, 5, 6, 8, 100, 200)), "pairs overlapping (5,5) (5,6)");
+  expect_eq(2, solver(example(&tc, 2, 7, /**/ 4, 5, 5, 6, 8, 100, 200)), "2nd pair has better option");
 
 
   for(int i = 0; i < 10; i++)
-    expect_eq(23, solver(1, example(&tc, 9, 40, /**/ 1, 8, 10, 16, 19, 22, 27, 33, 36, 40, 47, 52, 56, 61, 63, 71, 72, 75, 81, 81, 84, 88, 96, 98,
+    expect_eq(23, solver(example(&tc, 9, 40, /**/ 1, 8, 10, 16, 19, 22, 27, 33, 36, 40, 47, 52, 56, 61, 63, 71, 72, 75, 81, 81, 84, 88, 96, 98,
             103, 110, 113, 118, 124, 128, 129, 134, 134, 139, 148, 157, 157, 160, 162, 164)), "final test");
 
   printf("Done.\n");
   return;
 }
 
+TestCase tc;
 int main(int argc, const char * argv[]) {
-  test_all();
+  //test_all();
+  char line[5000];
+  memset(&tc, 0, sizeof(TestCase));
+  int count;
+  scanf("%d", &count);
+  for(int i = 0; i < count; i++) {
+    scanf("%d %d", &tc.people_count, &tc.chopstick_count);
+    tc.people_count += 8;
+    for(int j = 0; j < tc.chopstick_count; j++) {
+      scanf("%d", &tc.chopstick_lengths[j]);
+    }
+    printf("%d\n", solver(&tc));
+  }
   return 0;
 }
