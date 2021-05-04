e7c0e3de352ee68bfe9dd1c726fe8ed8e97dcdd5
Terry Yin <terry.yinzhe@gmail.com>
2021-05-04 12:20:52 +0800

    where is this leading too?

--- tennis_ruby3/tennis_spec.rb
+++ tennis_ruby3/tennis_spec.rb
@@ -7,20 +7,21 @@ class TennisGame
   end
 
   RULES = {
-      '0 0' => {player1: '1 0', player2: '0 1'},
-      '1 0' => {player1: '2 0', player2: '1 1'},
-      '0 1' => {player1: '1 1', player2: '0 2'},
-      '2 0' => {player1: '3 0', player2: '2 1'},
-      '0 2' => {player1: '1 2', player2: '0 3'},
-      '1 1' => {player1: 'Thirty Fifteen', player2: 'Fifteen Thirty'},
-      'Thirty Fifteen' => { player1: 'Forty Fifteen', player2:'Thirty All'},
-      'Fifteen Thirty' => { player1: 'Thirty All', player2:'Fifteen Forty'},
-      'Thirty All' => {player1: 'Forty Thirty', player2: 'Thirty Forty'},
-      'Forty Thirty' => { player1: 'Player One Wins', player2:'Deuce'},
-      'Thirty Forty' => { player1: 'Deuce', player2:'Player Two Wins'},
-      'Deuce' => {player1: 'Advantage Player One', player2: 'Advantage Player Two'},
-      'Advantage Player One' => {player1: 'Player One Wins', player2: 'Deuce'},
-      'Advantage Player Two' => {player1: 'Deuce', player2: 'Player Two Wins'},
+    '0 0' => {player1: '1 0', player2: '0 1'},
+    '1 0' => {player1: '2 0', player2: '1 1'},
+    '0 1' => {player1: '1 1', player2: '0 2'},
+    '2 0' => {player1: '3 0', player2: '2 1'},
+    '0 2' => {player1: '1 2', player2: '0 3'},
+    '1 1' => {player1: '2 1', player2: '1 2'},
+    '2 1' => { player1: '3 1', player2:'2 2'},
+    '1 2' => { player1: '2 2', player2:'Fifteen Forty'},
+    '3 1' => { player1: 'Player One Wins', player2: '3 2'},
+    '2 2' => {player1: 'Forty Thirty', player2: 'Thirty Forty'},
+    'Forty Thirty' => { player1: 'Player One Wins', player2:'Deuce'},
+    'Thirty Forty' => { player1: 'Deuce', player2:'Player Two Wins'},
+    'Deuce' => {player1: 'Advantage Player One', player2: 'Advantage Player Two'},
+    'Advantage Player One' => {player1: 'Player One Wins', player2: 'Deuce'},
+    'Advantage Player Two' => {player1: 'Deuce', player2: 'Player Two Wins'},
   }.freeze
 
   NAMES = {
@@ -30,48 +31,52 @@ class TennisGame
     '1 1' => 'Fifteen All',
     '2 0' => 'Thirty Love',
     '0 2' => 'Love Thirty',
+    '2 1' => 'Thirty Fifteen',
+    '1 2' => 'Fifteen Thirty',
+    '2 2' => 'Thirty All',
+    '3 1' => 'Forty Fifteen',
   }
   def player1_score = @score = RULES[@score][:player1]
-  def player2_score = @score = RULES[@score][:player2]
+    def player2_score = @score = RULES[@score][:player2]
 
-  def score
-    return NAMES[@score] if NAMES.keys.include?(@score)
-    @score
-  end
-end
-
-describe TennisGame do
-  let(:game) { TennisGame.new }
-  [
-    [0, 0, "Love All"],
-    [1, 0, "Fifteen Love"],
-    [0, 1, "Love Fifteen"],
-    [1, 1, "Fifteen All"],
-    [2, 1, "Thirty Fifteen"],
-    [2, 2, "Thirty All"],
-    [3, 2, "Forty Thirty"],
-    [3, 3, "Deuce"],
-    [4, 3, "Advantage Player One"],
-    [3, 4, "Advantage Player Two"],
-    [5, 3, "Player One Wins"],
-    [3, 5, "Player Two Wins"],
-    [4, 4, "Deuce"],
-    [4, 5, "Advantage Player Two"],
-    [4, 6, "Player Two Wins"]
-  ].each do |player1_balls, player2_balls, expectation|
-    it "when player1 wins #{player1_balls} and player2 wins #{player2_balls}, should say `#{expectation}`" do
-      (0...[player1_balls, player2_balls].min).each do
-        game.player1_score
-        game.player2_score
+      def score
+        return NAMES[@score] if NAMES.keys.include?(@score)
+        @score
       end
-      (0...(player1_balls - player2_balls)).each do
-        game.player1_score
-      end
-      (0...(player2_balls - player1_balls)).each do
-        game.player2_score
-      end
-      expect(game.score).to eq expectation
     end
-  end
-end
+
+    describe TennisGame do
+      let(:game) { TennisGame.new }
+      [
+        [0, 0, "Love All"],
+        [1, 0, "Fifteen Love"],
+        [0, 1, "Love Fifteen"],
+        [1, 1, "Fifteen All"],
+        [2, 1, "Thirty Fifteen"],
+        [2, 2, "Thirty All"],
+        [3, 2, "Forty Thirty"],
+        [3, 3, "Deuce"],
+        [4, 3, "Advantage Player One"],
+        [3, 4, "Advantage Player Two"],
+        [5, 3, "Player One Wins"],
+        [3, 5, "Player Two Wins"],
+        [4, 4, "Deuce"],
+        [4, 5, "Advantage Player Two"],
+        [4, 6, "Player Two Wins"]
+      ].each do |player1_balls, player2_balls, expectation|
+        it "when player1 wins #{player1_balls} and player2 wins #{player2_balls}, should say `#{expectation}`" do
+          (0...[player1_balls, player2_balls].min).each do
+            game.player1_score
+            game.player2_score
+          end
+          (0...(player1_balls - player2_balls)).each do
+            game.player1_score
+          end
+          (0...(player2_balls - player1_balls)).each do
+            game.player2_score
+          end
+          expect(game.score).to eq expectation
+        end
+      end
+    end
 
