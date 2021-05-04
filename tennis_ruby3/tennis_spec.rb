831f89dc08784dfb311f5409492a6b995d56be06
Terry Yin <terry.yinzhe@gmail.com>
2021-05-04 11:31:47 +0800

    another case

--- tennis_ruby3/tennis_spec.rb
+++ tennis_ruby3/tennis_spec.rb
@@ -9,7 +9,12 @@ class TennisGame
   end
 
   def player1_score
-    @score ='Fifteen Love'
+    if @score =='Fifteen All'
+      @score = 'Thirty Fifteen'
+    else
+
+      @score ='Fifteen Love'
+    end
   end
 
   def player2_score
@@ -22,24 +27,24 @@ class TennisGame
 end
 
 describe TennisGame do
+  let(:game) { TennisGame.new }
   [
     [0, 0, "Love All"],
     [1, 0, "Fifteen Love"],
     [0, 1, "Love Fifteen"],
     [1, 1, "Fifteen All"],
-#    [2, 1, "Thirty Fifteen"],
-#    [2, 2, "Thirty All"],
-#    [3, 2, "Forty Thirty"],
-#    [3, 3, "Deuce"],
-#    [4, 3, "Advantage Player One"],
-#    [5, 3, "Player One Wins"],
-#    [3, 5, "Player Two Wins"],
-#    [4, 4, "Deuce"],
-#    [4, 5, "Advantage Player Two"],
-#    [4, 6, "Player Two Wins"]
+    [2, 1, "Thirty Fifteen"],
+    #    [2, 2, "Thirty All"],
+    #    [3, 2, "Forty Thirty"],
+    #    [3, 3, "Deuce"],
+    #    [4, 3, "Advantage Player One"],
+    #    [5, 3, "Player One Wins"],
+    #    [3, 5, "Player Two Wins"],
+    #    [4, 4, "Deuce"],
+    #    [4, 5, "Advantage Player Two"],
+    #    [4, 6, "Player Two Wins"]
   ].each do |player1_balls, player2_balls, expectation|
     it "when player1 wins #{player1_balls} and player2 wins #{player2_balls}, should say `#{expectation}`" do
-      game = TennisGame.new
       (0...[player1_balls, player2_balls].min).each do
         game.player1_score
         game.player2_score
@@ -52,6 +57,10 @@ describe TennisGame do
       end
       expect(game.score).to eq expectation
     end
+  end
+
+  def lead_balls(player1_balls, player2_balls)
+
   end
 
 end
