f5c82555a29c99e2520062ab981058fdb761348c
Terry Yin <terry.yinzhe@gmail.com>
2021-05-04 16:22:36 +0800

    use name other than rule

--- tennis_ruby3/tennis_spec.rb
+++ tennis_ruby3/tennis_spec.rb
@@ -1,16 +1,13 @@
 # ruby 3.0.1
 #
 
+
 class TennisGame
   def initialize
     @score = [0, 0]
   end
 
   RULES = {
-    [1,3] => {player2: [0,4]},
-    [2,3] => {player2: [0,4]},
-    [3,1] => {player1: [4,0]},
-    [3,2] => {player1: [4,0]},
     [3,4] => {player1: [3,3], player2: [0,4]},
     [4,3] => {player1: [4,0], player2: [3,3]},
   }.freeze
@@ -23,15 +20,19 @@ class TennisGame
     [3,4] => 'Advantage Player Two',
     [4,3] => 'Advantage Player One',
     [4,0] => 'Player One Wins',
+    [4,1] => 'Player One Wins',
+    [4,2] => 'Player One Wins',
     [0,4] => 'Player Two Wins',
+    [1,4] => 'Player Two Wins',
+    [2,4] => 'Player Two Wins',
   }.freeze
 
 
-  def player1_score = @score = rule[:player1]
-  def player2_score = @score = rule[:player2]
+  def score!(player) = @score = rule[player]
   def score = name % { '0': 'Love', '1': 'Fifteen', '2': 'Thirty', '3': 'Forty' }
 
   private
+
   def rule = {player1: [@score[0] + 1, @score[1]], player2: [@score[0], @score[1] + 1]}.merge(RULES.fetch(@score, {}))
   def name = NAMES.fetch(@score, "%{#{@score[0]}} %{#{@score[1]}}")
 end
@@ -49,6 +50,7 @@ describe TennisGame do
     [3, 3, "Deuce"],
     [4, 3, "Advantage Player One"],
     [3, 4, "Advantage Player Two"],
+    [4, 2, "Player One Wins"],
     [5, 3, "Player One Wins"],
     [3, 5, "Player Two Wins"],
     [4, 4, "Deuce"],
@@ -57,14 +59,14 @@ describe TennisGame do
   ].each do |player1_balls, player2_balls, expectation|
     it "when player1 wins #{player1_balls} and player2 wins #{player2_balls}, should say `#{expectation}`" do
       (0...[player1_balls, player2_balls].min).each do
-        game.player1_score
-        game.player2_score
+        game.score!(:player1)
+        game.score!(:player2)
       end
       (0...(player1_balls - player2_balls)).each do
-        game.player1_score
+        game.score!(:player1)
       end
       (0...(player2_balls - player1_balls)).each do
-        game.player2_score
+        game.score!(:player2)
       end
       expect(game.score).to eq expectation
     end
