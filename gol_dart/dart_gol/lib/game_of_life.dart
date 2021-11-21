d10696c7b65a9d1ec83292af8502b7a5f6c63480
Terry Yin <terry.yinzhe@gmail.com>
2021-11-21 10:17:59 +0800

    neighbours

--- gol_dart/dart_gol/lib/game_of_life.dart
+++ gol_dart/dart_gol/lib/game_of_life.dart
@@ -1,5 +1,25 @@
 class ALife {
+  late final bool result;
+  ALife() {
+    result = false;
+  }
+
+  ALife.having(Neighbours neighbours) {
+    result = neighbours.count == 2;
+  }
+
   survive() {
-    return false;
+    return result;
+  }
+}
+
+class Neighbours {
+  final int count;
+  Neighbours(this.count);
+}
+
+extension NeighbourExtension on int {
+  Neighbours get neighbours {
+    return Neighbours(this);
   }
 }
--- gol_dart/dart_gol/test/gol_test.dart
+++ gol_dart/dart_gol/test/gol_test.dart
@@ -1,10 +1,18 @@
 import 'package:test/test.dart';
-import 'package:dart_gol/game_of_life.dart' show ALife;
+import 'package:dart_gol/game_of_life.dart' show ALife, NeighbourExtension;
 
 void main() {
   group('Game Of Life', () {
-    test('A life with no neighbour must die', () {
+    test('', () {
       expect(ALife().survive(), false);
+    });
+
+    test('', () {
+      expect(ALife.having(1.neighbours).survive(), false);
+    });
+
+    test('', () {
+      expect(ALife.having(2.neighbours).survive(), true);
     });
   });
 }
