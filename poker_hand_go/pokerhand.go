a9595119139b4462fd41a86e60154929b9e5a635
Terry Yin <terry.yinzhe@gmail.com>
2022-08-04 15:56:32 +0800

    go go go

---
+++ poker_hand_go/pokerhand.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	count := 0
+	ProcessLinesFromInput(func(game string) {
+		if Player1Win(game) {
+			count++
+		}
+	})
+	fmt.Println(fmt.Sprintf("%v", count))
+}
+
+func ProcessLinesFromInput(processor func(string)) {
+	buffer := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := buffer.ReadString('\n')
+		if len(input) == 0 {
+			break
+		}
+		processor(input)
+	}
+}
+
+type Hand struct {
+	cards string
+}
+
+func (h Hand) card(index int) int {
+	return rank(h.cards[index*3])
+}
+
+func (h Hand) Wins(other Hand) bool {
+	return h.card(4) > other.card(4)
+}
+
+func Player1Win(game string) bool {
+	h1 := Hand{cards: game[0:13]}
+	h2 := Hand{cards: game[15:]}
+	return h1.Wins(h2)
+}
+
+func rank(rank byte) int {
+	return strings.Index("23456789TJQKA", string(rank))
+}
