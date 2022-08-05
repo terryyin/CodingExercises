30dd42ec3729c103adbb25222d341dfba388b86e
Terry Yin <terry.yinzhe@gmail.com>
2022-08-05 12:27:18 +0800

    test data

--- poker_hand_go/pokerhand_app_test.go
+++ poker_hand_go/pokerhand_app_test.go
@@ -15,51 +15,47 @@ func Test_PokerHandsApp(t *testing.T) {
 
 	t.Run("should return 1 when player1 win the only game", func(t *testing.T) {
 		assert.Equal(t, "1\n", runPokerHandApp(
-			leastPowerfulHighCardWithHighest("8D")+" "+
-				leastPowerfulHighCardWithHighest("7D")))
+			game(hands.highCardEndWith("8D"), hands.highCardEndWith("7D"))))
 	})
 
 	t.Run("should return 0 when player1 lose the only game", func(t *testing.T) {
 		assert.Equal(t, "0\n", runPokerHandApp(
-			leastPowerfulHighCardWithHighest("7D")+" "+
-				leastPowerfulHighCardWithHighest("8D")))
+			game(hands.highCardEndWith("7D"), hands.highCardEndWith("8D"))))
 	})
 
 	t.Run("should return 2 when player1 win the only two games", func(t *testing.T) {
 		assert.Equal(t, "2\n", runPokerHandApp(
-			leastPowerfulHighCardWithHighest("8D")+" "+
-				leastPowerfulHighCardWithHighest("7D")+"\n"+
-				leastPowerfulHighCardWithHighest("8D")+" "+
-				leastPowerfulHighCardWithHighest("7D")))
+			game(hands.highCardEndWith("8D"), hands.highCardEndWith("7D"))+"\n"+
+				game(hands.highCardEndWith("8D"), hands.highCardEndWith("7D"))))
 	})
 }
 
-func leastPowerfulHighCardWithHighest(card string) string {
+type Example int
+
+const hands = Example(0)
+
+func (e Example) highCardEndWith(card string) string {
 	return "2H 3D 4S 5C 7C"[:14-len(card)] + card
 }
 
-func handWithOnePairOf(rank string) string {
+func (e Example) onePairOf(rank string) string {
 	return "KH QD 7S " + rank + "C " + rank + "D"
 }
 
-func threeOfAkind(rank string) string {
-	return "KH QD " + rank + "C " + rank + "D " + rank + "D"
+func (e Example) threeOfAkind(rank string) string {
+	return "KH QD " + rank + "C " + rank + "C " + rank + "D"
 }
 
-func handWithOrpanAndTwoPairs(orphan string, rank1 string, rank2 string) string {
+func (e Example) orpanAndTwoPairs(orphan string, rank1 string, rank2 string) string {
 	return orphan + "H " + rank1 + "C " + rank1 + "D " + rank2 + "C " + rank2 + "D"
 }
 
-func handWithTwoPairsOf(rank1 string, rank2 string) string {
-	return handWithOrpanAndTwoPairs("K", rank1, rank2)
+func (e Example) twoPairsOf(rank1 string, rank2 string) string {
+	return e.orpanAndTwoPairs("K", rank1, rank2)
 }
 
 func game(p1 string, p2 string) string {
 	return p1 + " " + p2
-}
-
-func highcardGame(p1Highest string, p2Highest string) string {
-	return game(leastPowerfulHighCardWithHighest(p1Highest), leastPowerfulHighCardWithHighest(p2Highest))
 }
 
 func runPokerHandApp(input string) string {
--- poker_hand_go/pokerhand_test.go
+++ poker_hand_go/pokerhand_test.go
@@ -7,61 +7,61 @@ import (
 )
 
 func Test_PokerHand(t *testing.T) {
-	assertWin := func(game string) {
-		assert.Truef(t, Player1Win(game), "game: %v", game)
+	assertWin := func(left string, right string) {
+		assert.Truef(t, Player1Win(game(left, right)), "game: %v vs %v", left, right)
 	}
-	assertNotWin := func(game string) {
-		assert.Falsef(t, Player1Win(game), "game: %v", game)
+	assertNotWin := func(left string, right string) {
+		assert.Falsef(t, Player1Win(game(left, right)), "game: %v vs %v", left, right)
 	}
 
 	t.Run("win by highest highcard", func(t *testing.T) {
-		assertWin(highcardGame("9D", "8D"))
-		assertWin(highcardGame("TD", "9D"))
-		assertWin(highcardGame("AD", "QD"))
+		assertWin(hands.highCardEndWith("9D"), hands.highCardEndWith("8D"))
+		assertWin(hands.highCardEndWith("TD"), hands.highCardEndWith("9D"))
+		assertWin(hands.highCardEndWith("AD"), hands.highCardEndWith("QD"))
 	})
 
 	t.Run("peace", func(t *testing.T) {
-		assertNotWin(highcardGame("9D", "9D"))
+		assertNotWin(hands.highCardEndWith("9D"), hands.highCardEndWith("9D"))
 	})
 
 	t.Run("win by highest highcard unordered", func(t *testing.T) {
-		assertWin(highcardGame("9D 6C", "8D 7C"))
+		assertWin(hands.highCardEndWith("9D 6C"), hands.highCardEndWith("8D 7C"))
 	})
 
 	t.Run("lose by highest highcard", func(t *testing.T) {
-		assertNotWin(highcardGame("8D", "9D"))
+		assertNotWin(hands.highCardEndWith("8D"), hands.highCardEndWith("9D"))
 	})
 
 	t.Run("win by 2nd highest highcard", func(t *testing.T) {
-		assertWin(highcardGame("8C 9D", "9D"))
+		assertWin(hands.highCardEndWith("8C 9D"), hands.highCardEndWith("9D"))
 	})
 
 	t.Run("win by one pair vs high card", func(t *testing.T) {
-		assertWin(game(handWithOnePairOf("2"), leastPowerfulHighCardWithHighest("AD")))
-		assertWin(game(handWithOnePairOf("8"), leastPowerfulHighCardWithHighest("AD")))
+		assertWin(hands.onePairOf("2"), hands.highCardEndWith("AD"))
+		assertWin(hands.onePairOf("8"), hands.highCardEndWith("AD"))
 	})
 
 	t.Run("lose by high card vs one pair", func(t *testing.T) {
-		assertNotWin(game( leastPowerfulHighCardWithHighest("AD"), handWithOnePairOf("2")))
+		assertNotWin(hands.highCardEndWith("AD"), hands.onePairOf("2"))
 	})
 
 	t.Run("one pair vs one pair", func(t *testing.T) {
-		assertNotWin(game(handWithOnePairOf("2"), handWithOnePairOf("3")))
-		assertWin(game(handWithOnePairOf("3"), handWithOnePairOf("2")))
-		assertWin(game("2H 2C 3D 4S AC", "2H 2C 3D 4S 5D"))
+		assertNotWin(hands.onePairOf("2"), hands.onePairOf("3"))
+		assertWin(hands.onePairOf("3"), hands.onePairOf("2"))
+		assertWin("2H 2C 3D 4S AC", "2H 2C 3D 4S 5D")
 	})
 
 	t.Run("two pairs vs one pair", func(t *testing.T) {
-		assertWin(game(handWithTwoPairsOf("2", "3"), handWithOnePairOf("4")))
+		assertWin(hands.twoPairsOf("2", "3"), hands.onePairOf("4"))
 	})
 
 	t.Run("two pairs vs two pairs", func(t *testing.T) {
-		assertNotWin(game(handWithTwoPairsOf("2", "3"), handWithTwoPairsOf("3", "4")))
-		assertWin(game(handWithOrpanAndTwoPairs("Q", "4", "5"), handWithTwoPairsOf("3", "5")))
+		assertNotWin(hands.twoPairsOf("2", "3"), hands.twoPairsOf("3", "4"))
+		assertWin(hands.orpanAndTwoPairs("Q", "4", "5"), hands.twoPairsOf("3", "5"))
 	})
 
 	t.Run("three of a kind", func(t *testing.T) {
-		assertWin(game(threeOfAkind("2"), handWithTwoPairsOf("3", "4")))
+		assertWin(hands.threeOfAkind("2"), hands.twoPairsOf("3", "4"))
 	})
 
 }
